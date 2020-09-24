e446969d1b364d5e78b65e191f178c30e0c2d5d9
Yuta Era <era.yuta@gmail.com>
2020-09-25 00:25:18 +0900

    Initial Commit

---
+++ omikuji.js
@@ -0,0 +1,31 @@
+let username = prompt("お名前を教えてください");
+let userresult;
+
+if (username === '') {
+  username = '名無し';
+};
+
+document.getElementById('name').innerHTML = username;
+
+
+let rand = Math.floor(Math.random() * 5);
+
+switch (rand) {
+  case 0:
+    userresult = '大吉';
+    break;
+  case 1:
+    userresult = '中吉';
+    break;
+  case 2:
+    userresult = '小吉';
+    break;
+  case 3:
+    userresult = '吉';
+    break;
+  case 4:
+    userresult = '凶';
+    break;
+};
+
+document.getElementById('luck').innerHTML = userresult;
